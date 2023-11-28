c1a258d18585fdc5f9a2c5777ed47f4f7df749ab
boisset-c <boissetcosme@gmail.com>
2023-11-28 17:49:37 -0600

    Passed fourth exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...targets) {
+	let answer = [];
+	targets = Array.isArray(targets) ? [...targets] : [...targets];
 
+	for (let i = 0; i < array.length; i++) {
+		if (!targets.includes(array[i])) {
+			answer.push(array[i]);
+		}
+	}
+	return answer;
 };
 
 // Do not edit below this line
