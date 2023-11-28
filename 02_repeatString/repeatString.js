0a2cfd849f69bb0716f306cc4aa9d2a30c9a1545
boisset-c <boissetcosme@gmail.com>
2023-11-28 17:04:33 -0600

    Passed second exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+	let repeatString = "";
+	let howManyTimes = times;
 
+	if (times < 0) return "ERROR";
+
+	for (let i = 0; i < howManyTimes; i++) {
+		repeatString += string;
+	}
+
+	return repeatString;
 };
 
 // Do not edit below this line
