e8dcc06da1b9f2d0e8bd71f498b77fa1ded1f21e
boisset-c <boissetcosme@gmail.com>
2023-11-28 17:20:28 -0600

    Passed third  exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function (string) {
+	let stringArray = string.split("");
+	let arrayLength = stringArray.length;
+	let reversedArray = [];
+	let answer;
 
+	for (let i = arrayLength - 1; i >= 0; i--) {
+		reversedArray.push(stringArray[i]);
+		console.log(stringArray);
+	}
+
+	answer = reversedArray.join("");
+	return answer;
 };
 
 // Do not edit below this line
