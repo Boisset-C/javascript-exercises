4f13a66aa6a5d157db445e9dfb481f4dc60d1b32
Boisset-c <boissetcosme@gmail.com>
2023-12-06 19:37:18 -0600

    Finished exercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  return people.reduce(
+    (oldest, person) => {
+      const personAge = getAge(person, currentYear);
+      const oldestAge = getAge(oldest, currentYear);
+
+      return personAge > oldestAge ? person : oldest;
+    },
+    { yearOfBirth: currentYear, yearOfDeath: currentYear }
+  ); // Initial value for reduce
 };
+
+function getAge(person, currentYear) {
+  if (!person.yearOfDeath) {
+    person.yearOfDeath = currentYear;
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
